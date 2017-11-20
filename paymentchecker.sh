c2b68a6a0d75097450b649bd1f0ab51912831c98
Awarrior91 <awarrior@hotmail.de>
2017-11-20 20:43:20 +0100

    some love

--- paymentchecker.sh
+++ paymentchecker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ID=0
+
 while getopts i: option
 do
  case "${option}"
@@ -6,4 +8,10 @@ do
  i) ID=${OPTARG};;
  esac
 done
+if [ $ID -eq 0 ]
+then
+ echo "No nodeid specified. Run script like this:./paymentchecker.sh -i yournodeid"
+ exit 2
+fi
 wget -q -O pay https://securenodes.eu.zensystem.io/api/grid/$ID/pmts && cat pay | grep -o -P '"zen":".{0,10}' | sed 's/\"zen\"\:\"//g' | sed 's/null//g' | perl -nle '$sum += $_ } END { print $sum'
+rm pay
