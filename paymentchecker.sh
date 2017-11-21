ffef208c0905c638686a8353d981576fcf90b2cd
Awarrior91 <awarrior@hotmail.de>
2017-11-21 21:06:59 +0100

    Update paymentchecker.sh

--- paymentchecker.sh
+++ paymentchecker.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 ID=0
+RG=""
 
-while getopts i: option
+while getopts i:r: option
 do
  case "${option}"
  in
  i) ID=${OPTARG};;
+ r) RG=${OPTARG};;
  esac
 done
+if [ $ID -eq 0 ] || [ $RG -eq 0 ]
+  then
+  echo "Run script like this: ./paymentchecker.sh -i nodeid -r regioncode"
+  exit 2
+fi
 if [ $ID -eq 0 ]
-then
- echo "No nodeid specified. Run script like this:./paymentchecker.sh -i yournodeid"
+ then
+ echo "No nodeid specified."
  exit 2
 fi
-wget -q -O pay https://securenodes.eu.zensystem.io/api/grid/$ID/pmts && cat pay | grep -o -P '"zen":".{0,10}' | sed 's/\"zen\"\:\"//g' | sed 's/null//g' | perl -nle '$sum += $_ } END { print $sum'
+if [ $RG -eq "" ]
+  then
+  echo "No region code specified. Region codes are: eu for Europe. na for North America. sea for Souteast Asia."
+  exit 2
+fi
+wget -q -O pay https://securenodes.$RG.zensystem.io/api/grid/$ID/pmts && cat pay | grep -o -P '"zen":".{0,10}' | sed 's/\"zen\"\:\"//g' | sed 's/null//g' | perl -nle '$sum += $_ } END { print $sum'
 rm pay
