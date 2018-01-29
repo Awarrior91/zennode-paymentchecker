9373ff4a57f1cf12aae1032487ebc6da8b553133
Awarrior91 <awarrior@hotmail.de>
2018-01-29 23:25:23 +0100

    Update paymentchecker.sh

--- paymentchecker.sh
+++ paymentchecker.sh
@@ -12,8 +12,11 @@ do
 done
 if [ $ID -gt 1 ]
   then
-    if [ "$RG" == "eu2" ] || [ "$RG" == "na2" ] || [ "$RG" == "sea" ] || [ "$RG" == "eu" ] || [ "$RG" == "na" ]
+    if [ "$RG" == "eu2" ] || [ "$RG" == "na2" ]
       then
+      wget -q -O zenpay$ID https://securenodes2.$RG.zensystem.io/api/grid/$ID/pmts && cat zenpay$ID | grep -o -P '"zen":".{0,10}' | sed 's/\"zen\"\:\"//g' | sed 's/null//g' | perl -nle '$sum += $_ } END { print $sum'
+      rm zenpay$ID
+      elif [ "$RG" == "sea" ] || [ "$RG" == "eu" ] || [ "$RG" == "na" ]
       wget -q -O zenpay$ID https://securenodes.$RG.zensystem.io/api/grid/$ID/pmts && cat zenpay$ID | grep -o -P '"zen":".{0,10}' | sed 's/\"zen\"\:\"//g' | sed 's/null//g' | perl -nle '$sum += $_ } END { print $sum'
       rm zenpay$ID
     fi
